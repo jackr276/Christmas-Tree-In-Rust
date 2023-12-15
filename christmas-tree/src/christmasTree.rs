50139bc9c8f4a3f1209e31e3e9624129e61234c9
jackr276 <jack_m_robbins@me.com>
2023-12-15 17:18:37 -0500

    added documentation

--- christmas-tree/src/christmasTree.rs
+++ christmas-tree/src/christmasTree.rs
@@ -11,14 +11,15 @@ use colored::*;
 fn main() {
     println!("{}{}{}{}", "\n", " ".repeat(20), "Merry Christmas!".truecolor(214,0,28).bold(), "\n");
     christmas_tree();
+    
+    //print out the trunk
     println!("{}{}", " ".repeat(27), "||".truecolor(101,67,33).bold());
     println!("{}{}{}", " ".repeat(27), "||".truecolor(101,67,33).bold(), "\n");
 }
 
-
+//Uses a reverse for loop to appropriately print out the "tree", asterisk ornaments and needed spaces
 fn christmas_tree(){
     for n in (0..27).rev(){
         println!("{}{}{}{}{}", " ".repeat(n), "*".yellow().bold(), "/".repeat(27-n).truecolor(0,110,51).bold(), "\\".repeat(27-n).truecolor(0,110,51).bold(), "*".yellow().bold());
-        //Add the spaces needed before the n
     }
 }
