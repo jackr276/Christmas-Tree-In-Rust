5cbc31d5e8b0b605e4946d3f2f09057ba97cb4b9
jackr276 <jack_m_robbins@me.com>
2023-12-15 16:54:41 -0500

    added gitignore

--- target/debug/deps/lazy_static-4b432e5b47262c38.d
+++
@@ -1,8 +0,0 @@
-/home/jackr/Programming/GitHub/Rust-Project/target/debug/deps/lazy_static-4b432e5b47262c38.rmeta: /home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
-
-/home/jackr/Programming/GitHub/Rust-Project/target/debug/deps/liblazy_static-4b432e5b47262c38.rlib: /home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
-
-/home/jackr/Programming/GitHub/Rust-Project/target/debug/deps/lazy_static-4b432e5b47262c38.d: /home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
-
-/home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs:
-/home/jackr/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs:
